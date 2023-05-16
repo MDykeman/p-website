92195f2ee1a0456d727fa5b2c2bfead9b6683024
MDykeman <matthewdykeman1@gmail.com>
2023-05-16 13:02:05 -0400

    Adding in hashtags to allow for a specific section to load first

--- js/cubeTransition.js
+++ js/cubeTransition.js
@@ -7,27 +7,16 @@
 		outClass, inClass, onGoing = false;
 	$('#cubeTransition>div:eq(0)').addClass('visible');
 
-	// var length = $('#cubeTransition>.page').length,
-	// 	current = 1,
-	// 	next = 1,
-	// 	outClass, inClass, onGoing = false;
-	// $('#cubeTransition>.page:eq(0)').addClass('visible');
 
-	// for (i = 0; i < length; i++) {
-	// 	var bullet = $("<a >About</a>");
-	// 	if (i == 0) bullet.addClass('active');
-	// 	$("#main-menu>ul>li>").append(bullet);
-	// }
-	// var bullet = $("<li><a href='#' class='active'>About</a></li><li><a href='#'>Work</a></li><li><a href='#'>Contact</a></li>");
 
 
 
 	for (i = 0; i < length; i++) {
 		var bullet;
-		if (i == 0) bullet = $("<li><a >About</a></li>");
+		if (i == 0) bullet = $("<li><a href='#about' >About</a></li>");
 		if (i == 0) bullet.addClass('active');
-		if (i == 1) bullet = $("<li><a >Work</a></li>");
-		if (i == 2) bullet = $("<li><a >Contact</a></li>");
+		if (i == 1) bullet = $("<li><a href='#work' >Work</a></li>");
+		if (i == 2) bullet = $("<li><a href='#contact' >Contact</a></li>");
 		$("#main-menu>ul").append(bullet);
 	}
 
